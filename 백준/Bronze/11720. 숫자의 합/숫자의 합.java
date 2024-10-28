f8435ddd67486ea297c37db953e98f3b17ca69e5
김가은 <127271876+Gaannini@users.noreply.github.com>
2024-10-28 20:30:37 +0900

    [Bronze IV] Title: 숫자의 합, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int sum = 0;
+    // 11720 : 숫자의 합 구하기
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-		String[] numarr = sc.next().split("");
-		for (int i = 0; i < N; i++) {
-			sum += Integer.parseInt(numarr[i]);
-		}
-		System.out.println(sum);
-	}
+        // N값 입력받기
+        int N = sc.nextInt();
+
+        // N값 길이만큼의 String형 변수 저장
+        String Snum = sc.next();
+
+        // String 변수를 char[] 배열에 저장
+        char[] cNum = Snum.toCharArray();
+
+        // 배열의 각 값을 더한다.
+        int sum = 0; // 숫자의 합을 저장할 변수
+
+        for(int i = 0; i < N ; i++){
+            sum += cNum[i] - '0'; // '1' = 49, '0' = 48
+        }
+        System.out.print(sum);
+    }
 }

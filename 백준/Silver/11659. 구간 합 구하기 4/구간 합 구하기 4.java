fd0adc35538b2e0614fe0bc03aa1bdd014ce655a
김가은 <127271876+Gaannini@users.noreply.github.com>
2024-10-29 02:01:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1120 ms, Memory: 59324 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,40 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int N = sc.nextInt();   // 주어질 숫자의 갯수
-        int M = sc.nextInt();   // 구간합 연산 갯수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] A = new int[N];   // 숫자들 배열
-        int[] S = new int[N];   // 구간 합 배열
-        int[][] Gugan = new int[M][2]; // 구간 저장
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        // 배열 채우기
-        for(int i=0;i<N;i++){
-          A[i] = sc.nextInt();
-        }
+        long[] S = new long[N+1];
 
-        // 구간 합 배열 채우기
-        for(int i=0;i<N;i++){
-            if(i==0) S[i] = A[i];
-            else S[i] = S[i-1] + A[i];
-        }
+        st = new StringTokenizer(br.readLine());
 
-        // 구간 받기
-        for(int i=0;i<M;i++){
-            Gugan[i][0] = sc.nextInt();
-            Gugan[i][1] = sc.nextInt();
+        // 배열, 구간합 배열 채우기
+        for(int i=1;i<N+1;i++){
+          S[i] = S[i-1] + Integer.parseInt(st.nextToken());
         }
 
         // 구간 합 구하고 출력 S[j] - S[i-1]
         for(int i=0;i<M;i++){
-            int fir = Gugan[i][0]-1;
-            int end = Gugan[i][1]-1;
-            if(fir==0) System.out.println(S[end]);
-            else System.out.println(S[end] - S[fir-1]);
+            st = new StringTokenizer(br.readLine());
+            int fir = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            System.out.println(S[end] - S[fir-1]);
         }
-
     }
 }

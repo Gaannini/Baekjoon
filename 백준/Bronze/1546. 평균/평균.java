41355911639e69a691deb874467b41c677969e21
김가은 <127271876+Gaannini@users.noreply.github.com>
2024-10-28 23:06:18 +0900

    [Bronze I] Title: 평균, Time: 216 ms, Memory: 18620 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] score = new int[N];
-		float[] newscore = new float[N];
-		float sum = 0;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-		for(int i=0;i<N;i++) {
-			score[i] = sc.nextInt();
-		}//점수 저장
-		int max = score[0];//최댓값
+        int N = sc.nextInt();
+        int[] A = new int[N];
 
-		for(int i=1;i<N;i++) {
-			if(score[i]>=max) {
-				max = score[i];
-			}
-		}//최댓값
+        for(int i = 0; i<N; i++){
+            A[i] = sc.nextInt();
+        }
+        long sum = 0;
+        long max = 0;
 
-		for(int i=0;i<N;i++) {
-			newscore[i] = (float)score[i]/max*100;
-			sum+=newscore[i];
-		}
-		System.out.print(sum/N);
-	}
+        // 최댓값 찾기
+        for(int i=0; i<N; i++){
+            if(A[i]>max) max = A[i];
+            sum += A[i];
+        }
+
+        //(각 점수 합) * 100 / 최대점수 / N
+        System.out.println(sum * 100.0 / max / N);
+    }
 }

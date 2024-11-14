b02fcd26ae9bbb926bb58582dcc60381da46ddcd
김가은 <127271876+Gaannini@users.noreply.github.com>
2024-11-14 20:17:17 +0900

    [Bronze I] Title: 이항 계수 1, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-	public static int factorial(int num) {
-		if (num == 0)
-			return 1;
-		else {
-			return num * factorial(num - 1);
-		}
-	}
+    public static void main(String[] args) {
+        //조합 점화식 : A[i][j] = A[i-1][j] + A[i-1][j-1]
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[][] DP = new int[N+1][N+1];
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int K = sc.nextInt();
+        //기본 초기화
+        for (int i = 0; i <= N; i++) {
+            DP[i][1] = i;
+            DP[i][0] = 1;
+            DP[i][i] = 1;
+        }
+        for(int i=3;i<=N;i++){
+            for(int j=2;j<=i;j++){
+                DP[i][j] = DP[i-1][j-1] + DP[i-1][j];
+            }
+        }
 
-		int result = factorial(N) / (factorial(K) * factorial(N - K));
-		System.out.println(result);
-	}
+        System.out.println(DP[N][K]);
+    }
 }
